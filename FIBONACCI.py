8c51a7c086c9ee2e1c2f60c09acf24bd92264210
SameeraNekkanti <sami.nekkanti@gmail.com>
2026-01-07 00:07:53 +0530

    Add files via upload

---
+++ FIBONACCI.py
@@ -0,0 +1,14 @@
+#fibonacci series
+n=int(input("enter no. of fibonaaci numbers to generate: "))
+a=0
+b=1
+for i in range(n):
+    print(a,end="")
+    a,b=b,a+b
+print()
+#celsius to farenheit
+'''
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit,"F")
+'''
